057cf6eebfad385c096545b2d42dbb68dd0fbd4c
shamruth <shamruth2822006@gmail.com>
2025-05-01 19:24:42 +0530

    added res status to check if vercel starts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set('view engine','ejs')
 var tasks=[];//an empty array
 app.get('/',(req,res)=>
 {
+    res.status(200).json('Welcome, your app is working well');
     res.render('login');
 })
 app.get('/todo',(req,res)=>
